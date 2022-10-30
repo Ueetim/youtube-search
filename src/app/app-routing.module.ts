ebd9555eb3da7b1ebc6e68e9a811681bb60936dd
uduak <lorduduak@gmail.com>
2022-10-30 19:51:52 +0100

    routing useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      {useHash: true, scrollPositionRestoration: "top"}
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
